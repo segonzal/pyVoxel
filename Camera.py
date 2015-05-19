965bc2ce2d80f611e1b59c749f33afdd5642598c
segonzal <seba-5@hotmail.com>
2015-05-19 00:13:01 -0300

    Add camera and input handlers

---
+++ Camera.py
@@ -0,0 +1,43 @@
+from OpenGL.GL import *
+import math
+
+class Camera:
+	def __init__(self):
+		self.x = 0.0
+		self.y = -2.5
+		self.z = -25.0
+		self._yaw = 0
+		self._pitch = 0
+
+	def yaw(self,dyaw):
+		self._yaw += dyaw
+
+	def pitch(self,dpitch):
+		self._pitch += dpitch
+
+	def walkForward(self,distance):
+		self.x -= distance * math.sin(math.radians(self._yaw))
+		self.z += distance * math.cos(math.radians(self._yaw))
+
+	def walkBackwards(self,distance):
+		self.x += distance * math.sin(math.radians(self._yaw))
+		self.z -= distance * math.cos(math.radians(self._yaw))
+
+	def strafeLeft(self,distance):
+		self.x -= distance * math.sin(math.radians(self._yaw-90))
+		self.z += distance * math.cos(math.radians(self._yaw-90))
+
+	def strafeRight(self,distance):
+		self.x += distance * math.sin(math.radians(self._yaw-90))
+		self.z -= distance * math.cos(math.radians(self._yaw-90))
+
+	def moveUp(self,distance):
+		self.y -= distance
+
+	def moveDown(self,distance):
+		self.y += distance
+
+	def lookThrough(self):
+		glRotate(self._pitch,1.0,0.0,0.0)
+		glRotate(self._yaw,0.0,1.0,0.0)
+		glTranslate(self.x,self.y,self.z)
---
+++ Input.py
@@ -0,0 +1,63 @@
+import pygame
+from pygame.locals import *
+
+class Input:
+	def __init__(self):
+		self.keydown = []
+		self.keyup = []
+		self.keyhold = {}
+
+		self.buttondown = []
+		self.buttonup = []
+		self.buttonhold = {}
+
+		self.isCloseRequested = False
+		self.mouseEntersScreen = lambda: None
+		self.mousefocus = False
+
+	def update(self):
+		self.keydown = []
+		self.keyup = []
+		self.buttondown = []
+		self.buttonup = []
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				self.isCloseRequested = True
+			if event.type == KEYDOWN:
+				self.keydown.append(event.key)
+				self.keyhold[event.key] = True
+			if event.type == KEYUP:
+				self.keyup.append(event.key)
+				self.keyhold[event.key] = False
+			if event.type == MOUSEBUTTONDOWN:
+				self.buttondown.append(event.button)
+				self.buttonhold[event.button] = True
+			if event.type == MOUSEBUTTONUP:
+				self.buttonup.append(event.button)
+				self.buttonhold[event.button] = False
+		
+		if not self.mousefocus and pygame.mouse.get_focused():
+			self.mouseEntersScreen()
+		self.mousefocus = pygame.mouse.get_focused()
+
+	def keyPress(self,code):
+		return code in self.keydown
+
+	def keyHold(self,code):
+		if code in self.keyhold:
+			return self.keyhold[code]
+		return False
+
+	def keyRelease(self,code):
+		return code in self.keyup
+
+	def buttonPress(self,code):
+		return code in self.buttondown
+
+	def buttonHold(self,code):
+		if code in self.buttonhold:
+			return self.buttonhold[code]
+		return False
+
+	def buttonRelease(self,code):
+		return code in self.buttonup
